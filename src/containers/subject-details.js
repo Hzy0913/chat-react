2edcba1aaac9723347033bf695be96d97056b9a0
Hzy0913 <758858816@qq.com>
2018-12-24 10:59:40 +0800

    feat(subject):  add  subject page

---
+++ src/containers/subject-details.js
@@ -0,0 +1,83 @@
+import React, {Component} from 'react';
+import {connect} from 'react-redux';
+import {Carousel, WingBlank, ListView} from 'antd-mobile';
+import {bindActionCreators} from 'redux';
+import QueueAnim from 'rc-queue-anim';
+import marked from 'marked';
+import highlight from 'highlight.js/lib/highlight';
+import javascript from 'highlight.js/lib/languages/javascript';
+import css from 'highlight.js/lib/languages/css';
+import php from 'highlight.js/lib/languages/php';
+import python from 'highlight.js/lib/languages/python';
+import sql from 'highlight.js/lib/languages/sql';
+import bash from 'highlight.js/lib/languages/bash';
+
+import * as authActions from '../redux/reduces/auth';
+import * as homeActions from '../redux/reduces/home';
+import Article from '../components/article';
+import '../assets/atom-one-light.css';
+import '../style/mark-style.styl';
+import {dateFormat} from '../utils';
+
+highlight.registerLanguage('javascript', javascript);
+highlight.registerLanguage('css', css);
+highlight.registerLanguage('php', php);
+highlight.registerLanguage('python', python);
+highlight.registerLanguage('sql', sql);
+highlight.registerLanguage('bash', bash);
+
+
+@connect(
+  state => ({auth: state.auth, home: state.home}),
+  dispatch => bindActionCreators({...authActions, ...homeActions}, dispatch),
+)
+export default class ArticleDetails extends Component {
+  state = {
+    article: {}
+  }
+  componentDidMount() {
+    const {match: {params: {id} = {}} = {}} = this.props;
+    axios.get(`/articleDetails/${id}`).then(res => {
+      this.setState({article: res.articleDetails});
+    });
+  }
+  render() {
+    const {
+      articleContent = '',
+      title,
+      label = [],
+      date
+    } = this.state.article || {};
+    marked.setOptions({
+      renderer: new marked.Renderer(),
+      gfm: true,
+      tables: true,
+      breaks: false,
+      pedantic: false,
+      sanitize: false,
+      smartLists: true,
+      smartypants: false,
+      highlight(code, lang) {
+        return highlight.highlightAuto(code).value;
+      }
+    });
+    const markHtml = marked(articleContent);
+    return (
+      <QueueAnim>
+        <div key="1">
+          <div className="detail-header">
+            <h1>{title}</h1>
+            <div className="detail-body-tag">
+              <div className="time">{dateFormat(new Date(date), 'yyyy-MM-dd')}</div>
+              {label.map(item => <span className="tag" key={item}>{item}</span>)}
+            </div>
+          </div>
+          <div
+            className="article-content"
+            dangerouslySetInnerHTML={{__html: markHtml}}
+          />
+        </div>
+      </QueueAnim>
+    );
+  }
+}
---
+++ src/containers/subject.js
@@ -0,0 +1,53 @@
+import React, {Component} from 'react';
+import {connect} from 'react-redux';
+import store from 'store';
+import QueueAnim from 'rc-queue-anim';
+import {Button} from 'antd-mobile';
+import {withRouter} from 'react-router-dom';
+import {bindActionCreators} from 'redux';
+
+import * as courseActions from '../redux/reduces/course';
+
+@connect(
+  state => ({allLearned: state.course.allLearned}),
+  dispatch => bindActionCreators(courseActions, dispatch)
+)
+class Learned extends Component {
+  state = {
+    learn: []
+  }
+  componentWillMount() {
+    const user = store.get('user') || {};
+    const {learn = []} = user;
+    learn.reverse();
+    this.setState({learn});
+  }
+  handleDetails = (id) => this.props.history.push(`/course-result/${id}`)
+  render() {
+    const {learn = []} = this.state;
+    return (
+      <QueueAnim>
+        <div key="animate">
+          {!learn.length ? <h1 className="result-tip">您还没有学习过的课程</h1> : null}
+          <QueueAnim>
+            {learn.map(item => (<div className="result-introduce learned" key={item._id} onClick={() => this.handleDetails(item._id)}>
+              <div className="result-left">
+                <h1>{item.title}</h1>
+                <h2>
+                  <p>积分：{item.score}</p>
+                  <p>标签：
+                    <span className="learned-tag">{item.tag}</span>
+                  </p>
+                </h2>
+              </div>
+              <div className="result-right">
+                <img src={item.bannerselect} />
+              </div>
+            </div>))}
+          </QueueAnim>
+        </div>
+      </QueueAnim>
+    );
+  }
+}
+export default withRouter(Learned);
