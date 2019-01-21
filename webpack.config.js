4271fc616927d1a8cac76361bf88c7a07106f5d3
Hzy0913 <758858816@qq.com>
2019-01-21 16:12:12 +0800

    feat(config):  remove cleanWebpackPlugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,6 @@
 const path = require('path');
 const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = {
   mode: 'development',
