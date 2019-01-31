ab0c12427fc86a8946489d990a05be060db05238
Hzy0913 <758858816@qq.com>
2019-01-31 14:00:46 +0800

    feat(package):  npm publish

--- example/chat.js
+++ example/chat.js
@@ -1,18 +1,45 @@
 import React, {Component} from 'react';
 import ChatReact from '../src';
+import VConsole from 'vconsole';
 // import ChatReact from '../lib/chat-react';
 // import {emojis} from '../components/chat/icon';
+// const vConsole = new VConsole();
 
 export default class Chat extends Component {
   state = {
     messages: [],
     timestamp: new Date().getTime()
   }
+  componentDidMount() {
+    const messages = [{"value": "123","timestamp":1548756977198,"userInfo":{"avatar":"http://img.binlive.cn/Fv0UOcmxAB7k_4JSWMzJLkgpZfUg","userId":"59e454ea53107d66ceb0a598"}},{"value":"1\n","timestamp":1548756979285,"userInfo":{"avatar":"http://img.binlive.cn/Fv0UOcmxAB7k_4JSWMzJLkgpZfUg","userId":"59e454ea53107d66ceb0a598"}},{"value":"1\n1","timestamp":1548756979622,"userInfo":{"avatar":"http://img.binlive.cn/Fv0UOcmxAB7k_4JSWMzJLkgpZfUg","userId":"59e454ea53107d66ceb0a598"}},{"value":"\n","timestamp":1548756979901,"userInfo":{"avatar":"http://img.binlive.cn/Fv0UOcmxAB7k_4JSWMzJLkgpZfUg","userId":"59e454ea53107d66ceb0a598"}},{"value":"1\n","timestamp":1548756980189,"userInfo":{"avatar":"http://img.binlive.cn/Fv0UOcmxAB7k_4JSWMzJLkgpZfUg","userId":"59e454ea53107d66ceb0a598"}},{"value":"1\n","timestamp":1548756980509,"userInfo":{"avatar":"http://img.binlive.cn/Fv0UOcmxAB7k_4JSWMzJLkgpZfUg","userId":"59e454ea53107d66ceb0a598"}},{"value":"1\n","timestamp":1548756980789,"userInfo":{"avatar":"http://img.binlive.cn/Fv0UOcmxAB7k_4JSWMzJLkgpZfUg","userId":"59e454ea53107d66ceb0a598"}},{"value":"1\n1","timestamp":1548756981093,"userInfo":{"avatar":"http://img.binlive.cn/Fv0UOcmxAB7k_4JSWMzJLkgpZfUg","userId":"59e454ea53107d66ceb0a598"}},{"value":"\n1","timestamp":1548756981397,"userInfo":{"avatar":"http://img.binlive.cn/Fv0UOcmxAB7k_4JSWMzJLkgpZfUg","userId":"59e454ea53107d66ceb0a598"}},{"value":"\n1","timestamp":1548756981684,"userInfo":{"avatar":"http://img.binlive.cn/Fv0UOcmxAB7k_4JSWMzJLkgpZfUg","userId":"59e454ea53107d66ceb0a598"}},{"value":"\n1","timestamp":1548756981949,"userInfo":{"avatar":"http://img.binlive.cn/Fv0UOcmxAB7k_4JSWMzJLkgpZfUg","userId":"59e454ea53107d66ceb0a598"}},{"value":"\n1","timestamp":1548756982206,"userInfo":{"avatar":"http://img.binlive.cn/Fv0UOcmxAB7k_4JSWMzJLkgpZfUg","userId":"59e454ea53107d66ceb0a598"}},{"value":"\n","timestamp":1548756982478,"userInfo":{"avatar":"http://img.binlive.cn/Fv0UOcmxAB7k_4JSWMzJLkgpZfUg","userId":"59e454ea53107d66ceb0a598"}},{"value":"1\n","timestamp":1548756982750,"userInfo":{"avatar":"http://img.binlive.cn/Fv0UOcmxAB7k_4JSWMzJLkgpZfUg","userId":"59e454ea53107d66ceb0a598"}},{"value":"1\n","timestamp":1548756983046,"userInfo":{"avatar":"http://img.binlive.cn/Fv0UOcmxAB7k_4JSWMzJLkgpZfUg","userId":"59e454ea53107d66ceb0a598"}},{"value":"1\n","timestamp":1548756983430,"userInfo":{"avatar":"http://img.binlive.cn/Fv0UOcmxAB7k_4JSWMzJLkgpZfUg","userId":"59e454ea53107d66ceb0a598"}}]
+
+    setTimeout(() => {
+      this.setState({messages, timestamp: new Date().getTime()});
+    }, 800)
+  }
   foucss = () => {
-    this.chat.refs.message.setScrollTop(1200);
+    const {messages} = this.state;
+    messages.push({"value": "123","timestamp": new Date().getTime(),"userInfo":{"avatar":"http://img.binlive.cn/Fv0UOcmxAB7k_4JSWMzJLkgpZfUg","userId":"59e454ea52107d66ceb0a598"}});
+    this.setState({messages, timestamp: new Date().getTime()});
+    // this.chat.refs.message.setScrollTop(600);
   }
   scrolltoUpper = (v) => {
-    console.log(v);
+    this.setState({loading: true});
+
+    const {messages} = this.state;
+    messages.unshift({
+      "value": "vvvvvvv",
+      "timestamp": 1448156977198,
+      "userInfo": {"avatar":"http://img.binlive.cn/Fv0UOcmxAB7k_4JSWMzJLkgpZfUg", "userId":"59e454ea52107d66ceb0a598"}
+    });
+    messages.unshift({
+      "value": "vvvvvvv2222",
+      "timestamp": 1448156977298,
+      "userInfo": {"avatar":"http://img.binlive.cn/Fv0UOcmxAB7k_4JSWMzJLkgpZfUg","userId":"59e454ea52107d66ceb0a598"}
+    });
+    setTimeout(() => {
+      this.setState({messages, timestamp: new Date().getTime(), loading: false});
+    }, 1222)
   }
   onScroll = (v) => {
     console.log(v);
@@ -25,17 +52,15 @@ export default class Chat extends Component {
     this.setState({messages, timestamp: new Date().getTime(), inputValue: ''});
   }
   render() {
-    const {inputValue = '', messages, timestamp} = this.state;
+    const {inputValue = '', messages, timestamp, loading} = this.state;
     const {className = ''} = this.props;
     const userInfo = {
       avatar: "http://img.binlive.cn/Fv0UOcmxAB7k_4JSWMzJLkgpZfUg",
       userId: "59e454ea53107d66ceb0a598"
     };
-    console.log(document.body.clientHeight);
-    console.log(document.documentElement.clientHeight);
     return (
       <div className={`chat-box ${className}`}>
-        <div onClick={this.foucss}>123123123</div>
+        <div onClick={this.foucss}>2121</div>
         <ChatReact
           dataSource={messages}
           userInfo={userInfo}
@@ -43,7 +68,7 @@ export default class Chat extends Component {
           sendMessage={this.sendMessage}
           timestamp={timestamp}
           placeholder="请输入"
-          messageListStyle={{width: '100%', height: window.outerHeight}}
+          messageListStyle={{width: '100%', height: 400, overflow: 'hidden'}}
           // customEmoticon={false}
           //not must
           textareaChange={this.textareaChange}
@@ -51,11 +76,12 @@ export default class Chat extends Component {
           timeBetween={0}
           timeagoMax={0}
           timeFormat="yyyy-MM-dd hh:mm"
-          // loading
+          loading={loading}
           // noData
           // noDataEle={<div>无数据</div>}
           scrolltoUpper={this.scrolltoUpper}
-          avatarClick={this.onScroll}
+          onScroll={this.onScroll}
+          // unreadCountChange={this.onScroll}
         />
       </div>
     );
